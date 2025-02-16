e3e2426ecf3395cbf773f222594181c21b683e0e
Kelly Li <kellyli838@gmail.com>
2025-02-16 13:44:59 -0500

    solved lesson 4

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,17 @@
 # Lesson 4
+import math
+
+cans = 0
+for i in range(3):
+    section = input()
+    cans += len(section)
+
+leftover = 0
+packs = 0 # packs of 12 cans
+
+packs = math.ceil(cans/12)
+leftover = int(packs)*12 - cans
+
+print(cans)
+print(leftover)
+print(packs*14.95)
