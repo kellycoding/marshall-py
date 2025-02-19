4697132db4fd14a495218cf3de9e9ddfde0bb9ec
Kelly Li <kellyli838@gmail.com>
2025-02-19 10:49:59 -0500

    solved lesson 7

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,14 @@
-# Lesson 7
+from random import randrange
+
+try:
+    dc = int(input())
+except:
+    print("Please enter an integer")
+    quit()
+
+d20 = randrange(1, 21)
+
+if d20 >= dc:
+    print("You succeeded the ability check")
+else:
+    print("You did not succeed the ability check")
