05977d33b5dcd76fd8db829bf13832d9f3ba4f18
Kelly Li <kellyli838@gmail.com>
2025-02-19 10:38:47 -0500

    solved lesson 5

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,14 @@
-# Lesson 5
+balance = float(input())
+cookies = input()
+
+for i in range(len(cookies)):
+    if cookies[i] != "b" and cookies[i] != "c":
+        print(f"{cookies[i]} is not a valid input")
+        quit()
+
+big = cookies.count("b")
+normal = cookies.count("c")
+revenue = (2.00*big) + (1.25*normal)
+cost = (0.75*big) + (0.50*normal)
+
+print(f"We sold {big + normal} cookies. We made {revenue-cost} in profit. Our total balance is {balance + revenue - cost}.")
