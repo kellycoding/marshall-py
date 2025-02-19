0f4a622212e50b3206ea6e9343089a56f020930d
Kelly Li <kellyli838@gmail.com>
2025-02-19 13:24:20 -0500

    solved lesson 9

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,25 @@
-# Lesson 9
+from random import choice
+
+playerChoice = ""
+
+while not playerChoice in {"R", "P", "S"}:
+    playerChoice = input("Enter R for rock, P for paper, or S for scissors")
+
+computerChoice = choice(["R", "P", "S"])
+if playerChoice == computerChoice:
+    print("Tie")
+elif playerChoice == "R":
+    if computerChoice == "P":
+        print("AI wins")
+    else:
+        print("Player wins")
+elif playerChoice == "P":
+    if computerChoice == "R":
+        print("Player wins")
+    else:
+        print("AI wins")
+else:
+    if computerChoice == "R":
+        print("AI wins")
+    else:
+        print("Player wins")
