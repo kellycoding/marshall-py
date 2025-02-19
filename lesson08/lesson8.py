0db258d145622dfe2c830df8606525c9a2d4f99c
Kelly Li <kellyli838@gmail.com>
2025-02-19 10:57:54 -0500

    solved lesson 8

--- lesson08/lesson8.py
+++ lesson08/lesson8.py
@@ -1,1 +1,14 @@
-# Lesson 8
+for i in range(6):
+    games = input()
+    wins = games.count("W")
+    
+    if wins == 0:
+        print("The player is eliminated")
+    else:
+        if wins < 3:
+            group = 3
+        elif wins < 5:
+            group = 2
+        else:
+            group = 1
+        print(f"The player is placed in group {group}")
