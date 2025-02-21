0b2516f49823266ef0871e130c4dcd29b40f7cfd
Kelly Li <kellyli838@gmail.com>
2025-02-21 14:48:02 -0500

    solved lesson 10

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,6 @@
-# Lesson 10
+number = input() + input() + input() + input()
+
+if number[0] in {"8", "9"} and number[1] == number[2] and number[3] in {"8", "9"}:
+    print("Telemarketer")
+else:
+    print("Not telemarketer")
