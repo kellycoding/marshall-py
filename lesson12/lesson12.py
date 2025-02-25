7875e79b0e388dc79f22c8da8f839f2ae322757e
Kelly Li <kellyli838@gmail.com>
2025-02-25 10:40:58 -0500

    solved lesson 12

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,10 @@
-# Lesson 12
+a1, a2, a3 = int(input("Enter angle 1: ")),int(input("Enter angle 2: ")),int(input("Enter angle 3: "))
+
+if a1+a2+a3 != 180:
+    print("You did not input a proper triangle")
+elif a1 == a2 == a3:
+    print("Equilateral")
+elif a1 != a2 != a3 and a3 != a1:
+    print("Scalene")
+else:
+    print("Isosceles")
