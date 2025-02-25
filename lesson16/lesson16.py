6269a2340a6d0ed64fd85ebc1f1460e2398bf959
Kelly Li <kellyli838@gmail.com>
2025-02-25 14:48:08 -0500

    solved lesson 16

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,9 @@
-# Lesson 16
+for i in range(50):
+    if (i+1)%15 == 0:
+        print("FizzBuzz")
+    elif (i+1)%5 == 0:
+        print("Buzz")
+    elif (i+1)%3 == 0:
+        print("Fizz")
+    else:
+        print(i+1)
