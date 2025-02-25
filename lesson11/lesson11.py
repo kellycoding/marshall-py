a0bfcc9bd51aa9b203571d7c4f03807690e1c2ef
Kelly Li <kellyli838@gmail.com>
2025-02-25 10:32:14 -0500

    solved lesson 11

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,16 @@
-# Lesson 11
+point = list(map(int, input("Please enter two coordinates: ").split()))
+
+if len(point) != 2:
+    print("Pleasse enter only two coordinate values")
+elif not point[0] or not point[1]:
+    print("The coordinate is not in a quadrant")
+elif point[0] > 0:
+    if point[1] > 0:
+        print("Quadrant 1")
+    else:
+        print("Quadrant 4")
+else:
+    if point[1] > 0:
+        print("Quadrant 2")
+    else:
+        print("Quadrant 3")
