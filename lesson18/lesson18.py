49df3d077c0bb3b27b2b77977f51d1b057a50b6e
Kelly Li <kellyli838@gmail.com>
2025-02-26 10:03:11 -0500

    solved lesson 18

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,14 @@
-# Lesson 18
+import math
+
+num = int(input())
+
+stoppingNum = -1
+
+for i in range(1, num):
+    if stoppingNum == i:
+        break
+    if num%i == 0:
+        print(i)
+        if math.floor(num/i) != i:
+            print(math.floor(num/i))
+        stoppingNum = math.floor(num/i)
