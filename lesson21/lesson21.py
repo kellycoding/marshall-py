41c42f78f59a2690653734d9dfbff341c80c6f70
Kelly Li <kellyli838@gmail.com>
2025-02-26 10:24:07 -0500

    solved lesson 21

--- lesson21/lesson21.py
+++ lesson21/lesson21.py
@@ -1,1 +1,15 @@
-# Lesson 21
+num = int(input())
+
+for i in range(1, num+1):
+    if i == 1:
+        maxFactors = 1
+        output = 1
+    else:
+        factors = 0
+        for factor in range(1, i+1):
+            if i%factor == 0:
+                factors += 1
+        if factors > maxFactors:
+            maxFactors = factors
+            output = i
+print(output)
