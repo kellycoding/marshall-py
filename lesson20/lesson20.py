0492bcc73220d9b8e1f33021692a6843aea31595
Kelly Li <kellyli838@gmail.com>
2025-02-26 10:16:22 -0500

    solved lesson 20

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,11 @@
-# Lesson 20
+totalSum = 0
+
+for num in range(1, 10001):
+    sum = 0
+    for i in range(1, num):
+        if num%i == 0:
+            sum += i
+    if sum == num:
+        totalSum += num
+
+print(totalSum)
