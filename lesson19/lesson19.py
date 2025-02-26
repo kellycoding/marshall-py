183401c8718b76bee3318dd7fc5ecdd0958afad5
Kelly Li <kellyli838@gmail.com>
2025-02-26 10:11:14 -0500

    solved lesson 19

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,27 @@
-# Lesson 19
+num = input()
+
+try:
+    int(num)
+except:
+    print("Not an integer")
+    quit()
+
+num = int(num)
+prime = True
+
+if num == 1:
+    prime = False
+elif num == 2:
+    pass
+elif num % 2 == 0:
+    prime = False
+else:
+    for i in range(3, num+1, 2):
+        if i != num and num%i == 0:
+            prime = False
+            break
+
+if prime == True:
+    print("Prime")
+else:
+    print("Not prime")
