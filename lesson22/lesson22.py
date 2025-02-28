97564112a288c2900b601ad94c371cb1b39bdd53
Kelly Li <kellyli838@gmail.com>
2025-02-28 14:42:58 -0500

    solved lesson 22

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,15 @@
-# Lesson 22
+n = int(input())
+
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    term1 = 0
+    term2 = 1
+    newTerm2 = -1
+    for i in range(n-1):
+        newTerm2 = term1 + term2
+        term1 = term2
+        term2 = newTerm2
+    print(term1)
