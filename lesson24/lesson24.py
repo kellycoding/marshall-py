62997b9151ea1539da109d8740a754be5d5f37a1
Kelly Li <kellyli838@gmail.com>
2025-02-28 14:49:47 -0500

    solved lesson 24

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,13 @@
-# Lesson 24
+longestLength = 0
+longestName = ""
+
+while True:
+    name = input()
+    if name == "X":
+        break
+    
+    if len(name) > longestLength:
+        longestLength = len(name)
+        longestName = name
+
+print(longestName)
