fddbbc15c0192400019524175942e6b9169279b2
Kelly Li <kellyli838@gmail.com>
2025-03-03 10:26:30 -0500

    solved lesson 27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,17 @@
-# Lesson 27
+def stringCleaner(text):
+    '''
+    arguments
+        text: string argument that is to be cleaned
+
+    returns
+        string object with only alphabetical lowercased characters
+    '''
+
+    result = ""
+    for character in text:
+        if character.isalpha():
+            result += character.lower()
+    return result
+
+value = stringCleaner(input())
+print(f"Value is: {value}")
