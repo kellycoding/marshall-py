392fb2a17ce97564ccff5b7a27b9a8c04bcd7c06
Kelly Li <kellyli838@gmail.com>
2025-03-03 10:39:39 -0500

    solved lesson 28

--- lesson28/lesson28.py
+++ lesson28/lesson28.py
@@ -1,1 +1,33 @@
-# Lesson 28
+def stringCleaner(text):
+    '''
+    arguments
+        text: string argument that is to be cleaned
+
+    returns
+        string object with only alphabetical lowercased characters
+    '''
+
+    result = ""
+    for character in text:
+        if character.isalpha():
+            result += character.lower()
+    return result
+
+def palindromeChecker(text):
+    '''
+    arguments
+        text: string argument that is to be checked if it is a palindrome
+
+    returns
+        True or False indicating whether or not the argument is a palindrome, not non-alphabetical characters or lower/uppercase
+    '''
+
+    text = stringCleaner(text)
+
+    for i in range(len(text)//2):
+        if text[i] != text[-1*i-1]:
+            return False
+            
+    return True
+
+print(palindromeChecker(input()))
