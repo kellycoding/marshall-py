e2932cf8cfe50e80f000c1620d1479ccf6e02453
Kelly Li <kellyli838@gmail.com>
2025-03-19 14:35:30 -0400

    solved lesson 30

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,10 @@
-# Lesson 30
+num = int(input())
+
+if num%2 == 0:
+    output = "10"*int((num/2))
+else:
+    output = "10"*int(((num+1)/2))
+    output = output[:-1]
+
+for i in range(num):
+    print(output[:i+1])
