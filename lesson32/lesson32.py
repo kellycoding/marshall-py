b7e4a54ddccb29a100b35c4359057c8e35153a1c
Kelly Li <kellyli838@gmail.com>
2025-03-21 14:36:39 -0400

    solved lesson 32

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,17 @@
-# Lesson 32
+str1, str2 = input(), input()
+characters = []
+
+if len(str1) > len(str2):
+    longerString = str1
+    shorterString = str2
+    stringLength = len(str1)
+else:
+    longerString = str2
+    shorterString = str1
+    stringLength = len(str2)
+
+for i in range(stringLength):
+    if longerString[i] in shorterString and not longerString[i] in characters:
+        characters.append(longerString[i])
+
+print(sorted(characters))
