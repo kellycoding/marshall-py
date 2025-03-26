5bf8cedf701fcadacc892c16914ab671d28a3268
Kelly Li <kellyli838@gmail.com>
2025-03-26 14:34:39 -0400

    solved lesson 37

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,22 @@
-# Lesson 37
+word = input()
+compressed = ""
+currentChar = ""
+charCount = 1
+
+for i in range(len(word)):
+    if not currentChar:
+        currentChar = word[i]
+    elif word[i] == currentChar:
+        charCount += 1
+    else:
+        compressed += currentChar
+        compressed += str(charCount)
+        currentChar = word[i]
+        charCount = 1
+
+compressed += currentChar
+compressed += str(charCount)
+if len(compressed) < len(word):
+    print(compressed)
+else:
+    print(word)
